b07a3bd8035d7355d446b5828588249f7445ca0e
dennismwaniki67 <dennismwaniki67@gmail.com>
2020-01-18 19:05:03 +0300

    added bootstap cdn in my index html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClient,HttpHeaders }from '@angular/common/http';
 import { AppComponent } from './app.component';
 
 import { ProfileComponent } from './components/profile/profile.component';
-
+import { ProfileService } from './services/profile.service';
 @NgModule({
   declarations: [
     AppComponent,
-    
     ProfileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ProfileService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
